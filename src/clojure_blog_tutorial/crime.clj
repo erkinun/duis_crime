fd42529cd06d29424244f4400b3891f1b91edff2
Erkin Unlu <erkin.unlu@rvu.co.uk>
2019-09-20 11:00:41 +0100

    reordering functions

--- src/clojure_blog_tutorial/crime.clj
+++ src/clojure_blog_tutorial/crime.clj
@@ -13,6 +13,12 @@
        :rows
        (into {})))
 
+(defn fips-code
+  "Given a county (a map with :fips_state_code and :fips_county_code keys),
+   returns the five-digit FIPS code for the county, as a string."
+  [county]
+  (str (:fips_state_code county) (:fips_county_code county)))
+
 (defn most-duis
   "Given a JSON filename of UCR crime data for a particular year, finds the
   counties with the most DUIs."
@@ -21,12 +27,7 @@
        load-json
        (sort-by :driving_under_influence)
        (take-last 10)
-       (map #(select-keys % [:driving_under_influence
-                             :fips_county_code
-                             :fips_state_code]))))
-
-(defn fips-code
-  "Given a county (a map with :fips_state_code and :fips_county_code keys),
-   returns the five-digit FIPS code for the county, as a string."
-  [county]
-  (str (:fips_state_code county) (:fips_county_code county)))
+       (map (fn [county]
+              [(fips (fips-code county))
+               (:driving_under_influence county)]))
+       (into {})))
